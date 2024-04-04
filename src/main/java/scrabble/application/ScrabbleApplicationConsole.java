bc6b6464d7c465ad21e5ce520f2fa96c8a4a0bdb
AntoineB0 <antoine.berteloot@etu.unilim.fr>
2024-04-04 09:42:05 +0200

    Initial Commit

---
+++ src/main/java/scrabble/application/ScrabbleApplicationConsole.java
@@ -0,0 +1,12 @@
+package scrabble.application;
+
+public class ScrabbleApplicationConsole {
+
+	public static void main(String[] args) {
+		 System.out.println("-------------------------------------------------------");
+		 System.out.println("-- Bienvenue dans notre magnifique jeu de scrabble ! --");
+		 System.out.println("-------------------------------------------------------");
+
+	}
+
+}
