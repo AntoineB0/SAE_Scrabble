bd10d0aa4b5e6b11808625e043e667ec0485de1f
JACOTIN, Bastien <bajacotin@gmail.com>
2024-06-09 17:09:36 +0200

    Reworked ScrabbleBoard to BoardView

---
+++ src/main/java/scrabble/view/BoardView.java
@@ -0,0 +1,75 @@
+package scrabble.view;
+
+
+import javafx.geometry.Pos;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+
+import scrabble.model.Board;
+
+
+
+public class BoardView  {
+    private Board board;
+    private GridPane boardPane;
+
+    public BoardView(Board board) {
+        this.board = board;
+        this.boardPane = createBoard();
+    }
+
+
+
+    public GridPane createBoard() {
+        GridPane boardPane = new GridPane();
+        boardPane.setHgap(6);
+        boardPane.setVgap(6);
+        boardPane.setGridLinesVisible(false);
+
+        for (int row = 0; row < board.getRows(); row++) {
+            for (int col = 0; col < board.getColumns(); col++) {
+                SquareView squareView = new SquareView(board.getSquare(row, col));
+                StackPane squarePane = squareView.createSquare();
+
+
+                boardPane.add(squarePane, col, row);
+
+                if (col == 7 && row == 7) {
+                    //add star
+
+                }
+            }
+        }
+
+        boardPane.setStyle("-fx-border-color: white;"
+                + " -fx-border-width: 10px;"
+                + " -fx-background-color: white;"
+                + "-fx-border-radius: 10;"
+                + "-fx-background-radius: 10;");
+        boardPane.setMaxSize(1000, 1000);
+        boardPane.setMinSize(1000, 1000);
+        boardPane.setAlignment(Pos.CENTER);
+
+        DropShadow dropShadow = new DropShadow();
+        boardPane.setEffect(dropShadow);
+        return boardPane;
+    }
+
+    public GridPane getBoardPane() {
+        return boardPane;
+    }
+
+    public void setBoardPane(GridPane boardPane) {
+        this.boardPane = boardPane;
+    }
+
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+}
